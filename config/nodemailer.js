0ffc6f859c0cab23f3fcabcbd40c57c2c318e577
AmanMishra0987 <aman0987mishra@gmail.com>
2025-11-14 16:36:33 +0530

    asd

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -5,12 +5,15 @@ dotenv.config();
 
 // Create reusable transporter object using Gmail app password
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // SSL
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
   },
 });
+
 
 // Verify connection configuration
 transporter.verify(function (error, success) {
